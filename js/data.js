97461fb999d9482efc4f017c3b61893460ee551c
Nazar Ivato <nazarivato@gmail.com>
2014-09-11 19:22:56 +0300

    Started introducing Angular.js into app

---
+++ js/data.js
@@ -0,0 +1,67 @@
+var data = {};
+
+data.tickets = [
+    {
+        id: 'RPG-0001',
+        summary: 'Have a morning cup of coffee and sigarette.',
+        status: 'todo',
+        assignee_id: 'nivato'},
+    {
+        id: 'RPG-0018',
+        summary: 'Investigate how to create some super-great web application',
+        status: 'inprogress',
+        assignee_id: 'apeterson'},
+    {
+        id: 'RPG-0003',
+        summary: 'Prepare design colors. Branding.',
+        status: 'review',
+        assignee_id: 'sgripson'},
+    {
+        id: 'RPG-0012',
+        summary: 'Investigate and learn super great Angular.js framework',
+        status: 'testing',
+        assignee_id: 'kdoherty'},
+    {
+        id: 'RPG-0010',
+        summary: 'Get familiar with Bootstrap css framework and different styles',
+        status: 'done',
+        assignee_id: 'jsimpson'},
+];
+
+data.people = [
+    {
+        username: 'nivato',
+        first_name: 'Nazar',
+        last_name: 'Ivato',
+        email: 'nazarivato@gmail.com',
+        picture: 'nazik.jpg',
+        color: 'red'},
+    {
+        username: 'apeterson',
+        first_name: 'Alex',
+        last_name: 'Peterson',
+        email: 'alexpeterson@gmail.com',
+        picture: 'alex.png',
+        color: 'green'},
+    {
+        username: 'sgripson',
+        first_name: 'Sonia',
+        last_name: 'Gripson',
+        email: 'soniagripson@gmail.com',
+        picture: 'sonia.png',
+        color: 'blue'},
+    {
+        username: 'kdoherty',
+        first_name: 'Kris',
+        last_name: 'Doherty',
+        email: 'krisdoherty@gmail.com',
+        picture: 'kris.png',
+        color: 'yellow'},
+    {
+        username: 'jsimpson',
+        first_name: 'Jessica',
+        last_name: 'Simpson',
+        email: 'jessicasimpson@gmail.com',
+        picture: 'jessica.png',
+        color: 'white'},
+];
--- js/kanban.js
+++ js/kanban.js
@@ -1,5 +1,30 @@
-function start(){
-    console.log('Application Successfully Started!');
+function enrichTickets(){
+    var color_class_mapping = {
+        red: 'list-group-item-danger',
+        green: 'list-group-item-success',
+        blue: 'list-group-item-info',
+        yellow: 'list-group-item-warning',
+        white: ''
+    };
+    for (var t = 0; t < data.tickets.length; t++){
+        var ticket = data.tickets[t];
+        for (var p = 0; p < data.people.length; p++){
+            var person = data.people[p];
+            if (ticket.assignee_id == person.username){
+                ticket.assignee = person;
+                ticket.css_class = color_class_mapping[person.color];
+                break;
+            }
+        }
+    }
 }
 
-window.onload = start;
+(function(){
+    var app = angular.module('Kanban', []);
+    
+    app.controller('SprintController', function(){
+        enrichTickets();
+        this.tickets = data.tickets;
+    });
+    
+})();
