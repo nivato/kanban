a1495808b4099e9ece080616fb218c1ee1a9dd64
Nazar Ivato <nazarivato@gmail.com>
2014-09-18 18:53:37 +0300

    Improved Mock

--- js/kanban-mocked.js
+++ js/kanban-mocked.js
@@ -1,8 +1,6 @@
 (function(){
     var app = angular.module('KanbanMocked', ['Kanban', 'ngMockE2E']);
     app.run(function($httpBackend){
-        var people = loadDataFrom('/json/people.json');
-        var tickets = loadDataFrom('/json/tickets.json');
         
         function loadDataFrom(filePath){
             var request = new XMLHttpRequest();
@@ -11,9 +9,30 @@
             return request.response;
         }
         
-        $httpBackend.whenGET(/\/people/).respond(people);
+        var people = loadDataFrom('/json/people.json');
+        var tickets = loadDataFrom('/json/tickets.json');
+        var authenticated = false;
         
-        $httpBackend.whenGET(/\/tickets/).respond(tickets);
+        
+        $httpBackend.whenGET(/\/people/).respond(function(method, url, data, headers){
+            // console.log(method);
+            // console.log(url);
+            // console.log(data);
+            // console.log(headers);
+            if (authenticated){
+                return [200, people, {}];
+            }
+            return [401, {
+                status: 'error', //ok|redirect|error
+                to: '',
+                html: '',
+                message: 'Unauthorized'
+            }, {}];
+        });
+        
+        $httpBackend.whenGET(/\/tickets/).respond(function(method, url, data, headers){
+            return [200, tickets, {}];
+        });
         
         $httpBackend.whenGET(/templates\/.*/).passThrough();
     });
