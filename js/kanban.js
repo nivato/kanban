7f2ee62ac46f7b76ac2a2dcb0fb26726737581fe
Nazar Ivato <nazarivato@gmail.com>
2014-09-23 19:41:18 +0300

    :white_check_mark: Implemented Routing :white_check_mark: Improved Angular structure

--- js/kanban.js
+++ js/kanban.js
@@ -1,50 +1,57 @@
 (function(){
     //var user = {};
     var user = { "username": "nivato", "first_name": "Nazar", "last_name": "Ivato", "picture": "nazik.jpg", "color": "red"};
-    var app = angular.module('Kanban', ['Sprint', 'ngRoute']);
+    var app = angular.module('Kanban', ['ngRoute']);
     
-    app.config(['$routeProvider', function($routeProvider){
+    app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+        $locationProvider.html5Mode(true);
         $routeProvider
             .when('/', {
                 templateUrl: '/templates/sprint-board.html',
-                controller: ['$http', function($http){
-                    var sprint = this;
-                    this.tickets = [];
-                    
-                    $http.get('/tickets').success(function(tckts){
-                        sprint.tickets = tckts;
-                    });
-                    
-                    this.numberOfTicketsWithStatus = function(status){
-                        var num = 0;
-                        for (var t = 0; t < this.tickets.length; t++){
-                            if (this.tickets[t].status === status){
-                                num++;
-                            }
-                        }
-                        return num;
-                    };
-                }],
-            controllerAs: 'sprint'});
+                controller: 'boardController',
+                controllerAs: 'sprint'
+            })
+            .when('/backlog', {templateUrl: '/templates/backlog.html'})
+            .when('/profile', {templateUrl: '/templates/profile.html'})
+            .when('/search', {templateUrl: '/templates/search.html'})
+            .when('/sprints', {templateUrl: '/templates/sprints.html'})
+            .when('/team', {templateUrl: '/templates/team.html'})
+            .when('/notfound', {templateUrl: '/templates/notfound.html'})
+            .otherwise({redirectTo: '/'});
     }]);
     
     app.controller('ApplicationController', ['$http', function($http){
-        
         this.currentUser = function(){
             return user;
         };
-        
         this.logout = function(){
             user = {};
         };
-        
         this.authenticated = function(){
             return !!user.username;
         };
-        
     }]);
     
-    app.controller('LoginController', ['$http', function($http){
+    app.controller('boardController', ['$http', function($http){
+        var sprint = this;
+        this.tickets = [];
+        
+        $http.get('/tickets').success(function(data){
+            sprint.tickets = data;
+        });
+        
+        this.numberOfTicketsWithStatus = function(status){
+            var num = 0;
+            for (var t = 0; t < this.tickets.length; t++){
+                if (this.tickets[t].status === status){
+                    num++;
+                }
+            }
+            return num;
+        };
+    }]);
+    
+    app.controller('loginController', ['$http', function($http){
         this.username = '';
         this.password = '';
         this.error = '';
@@ -81,32 +88,26 @@
         return {
             restrict: 'E',
             templateUrl: '/templates/login-form.html',
-            controller: ['$http', function($http){
-                this.username = '';
-                this.password = '';
-                this.error = '';
-                
-                this.submitLogin = function(){
-                    var loginForm = this;
-                    var dt = {
-                        username: this.username,
-                        password: this.password
-                    };
-                    this.username = '';
-                    this.password = '';
-                    this.error = '';
-                    $http.post('/login', dt)
-                        .success(function(data){
-                            user = data;
-                            loginForm.username = '';
-                            $('#loginForm').modal('hide');
-                        })
-                        .error(function(data, status, headers, config){
-                            loginForm.error = data.message;
-                        });
-                };
-            }],
+            controller: 'loginController',
             controllerAs: 'lgn'
+        };
+    });
+    
+    app.directive('boardTicket', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/board-ticket.html',
+        };
+    });
+    
+    app.filter('limitStrTo', function() {
+        return function(input, limit) {
+            input = input || '';
+            var out = input;
+            if (input.length > limit){
+                out = input.substring(0, limit) + '...';
+            }
+            return out;
         };
     });
     
